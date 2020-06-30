a8632c436836a545f1f5e9f8b37f7bdcd00109ef
Shawn Tompke <shawntompke@Shawns-MBP.routerlogin.net>
2020-06-29 19:59:53 -0700

    Added Account component

---
+++ front-end/auto-record-app/src/components/Account.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Account = () => {
+
+    return (
+        <>
+        <h1>Your Account</h1>
+        <h2>Email: </h2>
+        <h2>Cars: </h2>
+        </>
+    )
+}
+
+export default Account
