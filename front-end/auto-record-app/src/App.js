b69c77787dae537b7484a4976ba31caa20f23729
Shawn Tompke <shawntompke@Shawns-MacBook-Pro.local>
2020-07-02 16:20:27 -0700

    added react-router-dom

--- front-end/auto-record-app/src/App.js
+++ front-end/auto-record-app/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Route, Link } from 'react-router-dom';
 
 // Components
 import Account from './components/Account'
@@ -8,7 +9,9 @@ import Account from './components/Account'
 function App() {
   return (
     <div className="App">
+      <Link to='/account'>Account</Link>
       <header className="App-header">
+        <Route path='/account' component={Account} />
       <h1>Auto Record</h1>
       </header>
     </div>
